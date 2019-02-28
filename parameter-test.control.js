a577b9c9b2f89cef88dda36e263d768d7686631f
Charles Holbrow <CharlesHolbrow@gmail.com>
2019-02-28 14:45:01 -0500

    add comments with notes

--- parameter-test.control.js
+++ parameter-test.control.js
@@ -1,10 +1,14 @@
 loadAPI(7);
 
-// Remove this if you want to be able to use deprecated methods without causing script to stop.
-// This is useful during development.
+// Remove this if you want to be able to use deprecated methods without causing
+// script to stop. This is useful during development.
 host.setShouldFailOnDeprecatedUse(true);
 
-host.defineController("Charles", "parameter-test", "0.1", "49f8883d-59dc-4aac-9cb9-6430ac00799d", "audioishi");
+host.defineController("Charles",
+   "parameter-test",
+   "0.1",
+   "49f8883d-59dc-4aac-9cb9-6430ac00799d",
+   "audioishi");
 
 var SIZE = 8;
 
@@ -31,8 +35,21 @@ function init() {
    track.name().addValueObserver(function(name) {
       println('Track renamed: ' + name);
       for (var i = 0; i < SIZE; i++){
-         var p = rcCursor.getParameter(i);
-         println('remote controls parameter name:' + p.name().get());
+         // 'rc' is a com.bitwig.extension.controller.api.RemoteControl
+         // some useful methods are inherited from 'SettableRangedValue'
+         // (RangedValue have an upper/lower limit)
+         // - set(value [, resolution])
+         // - setRaw(value [, resolution])
+         // - setImmediately(value)
+         // - modulatedValue() // returns a RangedValue
+         // - reset()
+         // - value() // returns a SettableRangedValue
+         // What is the difference between:
+         // - .name().set(name) // This is displayed in the UI
+         // - .setLabel(label)  // I'm not sure what this is for - there is no "getLabel"
+         var rc = rcCursor.getParameter(i);
+
+         println('remote controls parameter name:' + rc.name().get());
       }
    });
 }
